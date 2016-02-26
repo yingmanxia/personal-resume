f97811d575bce5722d4c88fb21b6eab76bab3a1d
wanghan <yc@likyh.com>
2016-02-26 13:59:05 +0800

    完善

---
+++ script/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	var $tabs = $("#tabs-ul li");
+	var $wrapper=$(".wrapper");
+	$tabs.click(function () {
+        var $this= $(this);
+        var $n = $this.index();
+        // console.log($n);
+        var topNum="-"+$n * 460+"px";
+        // console.log(topNum);
+        $wrapper.animate({top:topNum},400);
+        $this.addClass('current');
+    	$tabs.not($this).removeClass('current');
+    	
+    });
+
+});
